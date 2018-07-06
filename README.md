eccd631d05360ec37ea86d75525c7f4afde6e4b1
Simon Weller <s.meierbeck@gmail.com>
2018-07-06 23:31:21 +0200

    Added basic security set-up for admin view and README.md

---
+++ README.md
@@ -0,0 +1,11 @@
+#Local set-up
+- clone the repo
+- create a copy of docker-compose.override.dev.yml, rename to docker-compose.override.yml and tweak if needed
+- create a copy of .env.dist, rename to .env and update MAILER_URL and DATABASE_URL
+- run docker-compose up -d to build docker containers and bring them up
+- run docker-compose exec -u dev php bash to get into the php container as user dev.
+- inside the container, cd sf4 and run composer install
+
+#Accessing the admin page
+- to access the admin menu, add a user with an encrypted password to the database (by hand for now)
+- to encrypt the password run bin/console security:encode password
---
+++ src/Entity/User.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Serializable;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Table(name="app_users")
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User implements UserInterface, Serializable
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=25, unique=true)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=64)
+     */
+    private $password;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function serialize()
+    {
+        return serialize([
+            $this->id,
+            $this->username,
+            $this->password,
+        ]);
+    }
+
+    public function unserialize($serialized)
+    {
+        list (
+            $this->id,
+            $this->username,
+            $this->password,
+        ) = unserialize($serialized, ['allowed_classes' => false]);
+    }
+
+    public function getRoles(): array
+    {
+        return ['ROLE_ADMIN'];
+    }
+
+    public function getSalt()
+    {
+        return null;
+    }
+
+    public function eraseCredentials()
+    {
+    }
+}
