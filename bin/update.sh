ca24dfb6e17714dd3ad5d96a4217241136ee5ef0
Simon Weller <s.meierbeck@gmail.com>
2018-07-07 11:13:35 +0200

    Added update.sh for one-step deploy

---
+++ bin/update.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+WORK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )"; [ -h "${WORK_DIR}/${0}" ] && DIR=readlink -n "${WORK_DIR}/${0}"
+
+composer="php /usr/local/bin/composer"
+
+echo "Current env: ${APP_ENV}"
+
+cd ${WORK_DIR}
+
+if [ "${APP_ENV}" == "prod" ]; then
+    $composer install --no-dev --optimize-autoloader
+    npm install --only=production
+    npm run build
+else
+    $composer install
+    npm install
+    npm run dev
+fi
+
+if [[ $* != *"--skip-migration"* ]]
+then
+    php bin/console doctrine:migrations:migrate --allow-no-migration --no-interaction
+    bin/console cache:clear --no-warmup
+fi
+
+if [ "${APP_ENV}" == "prod" ]; then
+    bin/console cache:warmup
+fi
