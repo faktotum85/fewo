18f66aeb879742d0bb6d31688c088cf298c00b79
Simon Weller <s.meierbeck@gmail.com>
2018-07-07 10:57:55 +0200

    Set up Encore and SCSS

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+// noinspection ES6ConvertVarToLetConst
+var Encore = require('@symfony/webpack-encore');
+
+Encore
+    .setOutputPath('public/build/')
+    .setPublicPath('/build')
+    .cleanupOutputBeforeBuild()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
+    .addEntry('js/app', './assets/js/app.js')
+    .addStyleEntry('css/app', './assets/css/app.scss')
+    .enableSassLoader()
+;
+
+module.exports = Encore.getWebpackConfig();
